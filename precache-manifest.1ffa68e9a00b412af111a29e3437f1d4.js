119c3c979379db5d70f03b3cd069ddfd256bcf4c
Austin Malerba <austin.malerba@gmail.com>
2020-11-23 20:00:44 -0700

    Updates

---
+++ precache-manifest.1ffa68e9a00b412af111a29e3437f1d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6750a6358c79a78a441dcf023e602a01",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ef02f7e11268f9d2b33",
+    "url": "./static/css/main.36d31be8.chunk.css"
+  },
+  {
+    "revision": "477bb2192d35f21fbda0",
+    "url": "./static/js/2.4a89c9f2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.4a89c9f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ef02f7e11268f9d2b33",
+    "url": "./static/js/main.190c70df.chunk.js"
+  },
+  {
+    "revision": "966243218629b3bdf6f8",
+    "url": "./static/js/runtime-main.209e6cb8.js"
+  }
+]);
