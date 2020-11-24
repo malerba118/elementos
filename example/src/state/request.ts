13265796e39b820469c712b0f799e1921f0e185a
Austin Malerba <austin.malerba@gmail.com>
2020-11-23 21:00:11 -0700

    refactor request manager

--- example/src/state/request.ts
+++ example/src/state/request.ts
@@ -7,50 +7,6 @@ enum Status {
   Rejected = 'rejected'
 }
 
-// export const createRequest$ = <T>(initialData: T) => {
-//   const status$ = atom(Status.Initial)
-//   const data$ = atom(initialData)
-//   const error$ = atom(null as Error | null)
-
-//   return molecule(
-//     {
-//       status: status$,
-//       data: data$,
-//       error: error$
-//     },
-//     {
-//       actions: ({ status, data, error }) => {
-//         const baseActions = {
-//           setPending: batched(() => {
-//             status.actions.set(Status.Pending)
-//             error.actions.set(null)
-//           }),
-//           setFulfilled: batched((result) => {
-//             status.actions.set(Status.Fulfilled)
-//             data.actions.set(result)
-//             error.actions.set(null)
-//           }),
-//           setRejected: batched((err) => {
-//             status.actions.set(Status.Rejected)
-//             error.actions.set(err)
-//           })
-//         }
-//       },
-//       deriver: ({ status, data, error }) => {
-//         return {
-//           isInitial: status === Status.Initial,
-//           isPending: status === Status.Pending,
-//           isFulfilled: status === Status.Fulfilled,
-//           isRejected: status === Status.Rejected,
-//           status,
-//           data,
-//           error
-//         }
-//       }
-//     }
-//   )
-// }
-
 export type CreateRequestOptions<T> = {
   defaultData?: T
 }
@@ -59,15 +15,11 @@ export const createRequest$ = <ExecutorParams extends any[], ExecutorReturn>(
   executor: (...args: ExecutorParams) => Promise<ExecutorReturn>,
   { defaultData }: CreateRequestOptions<ExecutorReturn> = {}
 ) => {
-  const status$ = atom(Status.Initial)
-  const data$ = atom(defaultData)
-  const error$ = atom(null as Error | null)
-
   return molecule(
     {
-      status: status$,
-      data: data$,
-      error: error$
+      status: atom(Status.Initial),
+      data: atom(defaultData),
+      error: atom(null as Error | null)
     },
     {
       actions: ({ status, data, error }) => {
